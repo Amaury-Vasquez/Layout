6e2ba2d79925f85b318eb707fbf2f90b6ed239db
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2020-10-26 13:06:51 -0600

    Updates

---
+++ precache-manifest.bd04f6f253caf84edc42bb2f2bf26dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dd0d600cb2edeb12f0b1c26020c419",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "05402cdabdc4acdce202",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.c50c5a90.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.c50c5a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0ef42f5c098f1a2fe7",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.d8b3088b.chunk.js"
+  },
+  {
+    "revision": "a829fd2af96ca710f641",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.7955c74b.js"
+  }
+]);
